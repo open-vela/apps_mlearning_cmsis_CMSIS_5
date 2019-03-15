c781847d6c1e84c6a3c829261acd1ea85908579b
openvela-robot <openvela-robot@xiaomi.com>
2019-03-15 16:47:52 +0800

    Bump pack version after release.
    Change-Id: Ifb9df18bfcc611a410f39711e4ed0d9f24fda0f7

--- docs/DSP/html/search/defines_0.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['_5f_5fpackq7',['__PACKq7',['../arm__math_8h.html#a3ebff224ad44c217fde9f530342e2960',1,'arm_math.h']]],
-  ['_5f_5fpkhbt',['__PKHBT',['../arm__math_8h.html#aa505179349146cf6186640b4c8b47714',1,'arm_math.h']]],
-  ['_5f_5fpkhtb',['__PKHTB',['../arm__math_8h.html#ac4be483b8d98e977a69baf4a292a76fe',1,'arm_math.h']]]
-];

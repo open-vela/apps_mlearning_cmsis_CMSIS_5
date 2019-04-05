8d8fb5e5ef5134fa9947bfaf35f332d1da79c44c
liujinye <liujinye@xiaomi.com>
2019-04-05 14:54:07 +0800

    SVDConv v3.3.26
    - fixed generation of __SAU_REGION_PRESENT (see git CMSIS: #561)
    - updated Copyright date 2019

--- CMSIS/NN/Include/arm_nnsupportfunctions.h
+++ CMSIS/NN/Include/arm_nnsupportfunctions.h
@@ -187,10 +187,10 @@ void arm_nn_mult_q7(
   uint32_t blockSize);
  
 /**
- * @brief macro for adding rounding offset
+ * @brief defition to adding rouding offset
  */
 #ifndef ARM_NN_TRUNCATE
-    #define NN_ROUND(out_shift) ( (0x1 << out_shift) >> 1 )
+    #define NN_ROUND(out_shift) ( 0x1 << (out_shift - 1) )
 #else
     #define NN_ROUND(out_shift) 0
 #endif

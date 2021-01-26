a0b3ad35deca5ec12f25e29c2977003fe8113bb7
liujinye <liujinye@xiaomi.com>
2021-01-26 15:23:01 +0800

    CoreValidation: Updat build config and script. - Add new build.py using python-matrix-builder module. - Rename model configs to reflect build target. - Add Cortex-M55 build targets. - Clang/GCC use compiler features instead of -mfpu explicitly - Remove disregarded flags

--- CMSIS/DSP/Include/arm_const_structs.h
+++ CMSIS/DSP/Include/arm_const_structs.h
@@ -30,9 +30,14 @@
 #ifndef _ARM_CONST_STRUCTS_H
 #define _ARM_CONST_STRUCTS_H
 
-#include "arm_math.h"
+#include "arm_math_types.h"
 #include "arm_common_tables.h"
+#include "dsp/transform_functions.h"
 
+#ifdef   __cplusplus
+extern "C"
+{
+#endif
    extern const arm_cfft_instance_f64 arm_cfft_sR_f64_len16;
    extern const arm_cfft_instance_f64 arm_cfft_sR_f64_len32;
    extern const arm_cfft_instance_f64 arm_cfft_sR_f64_len64;
@@ -73,4 +78,9 @@
    extern const arm_cfft_instance_q15 arm_cfft_sR_q15_len2048;
    extern const arm_cfft_instance_q15 arm_cfft_sR_q15_len4096;
 
+#ifdef   __cplusplus
+}
 #endif
+
+#endif
+
